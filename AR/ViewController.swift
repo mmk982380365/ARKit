d9795c8b8bd02122bfa8089748af9a226bb371db
realdata <982380365@qq.com>
2017-06-07 17:00:30 +0800

    add note

--- AR/ViewController.swift
+++ AR/ViewController.swift
@@ -17,8 +17,12 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        //AR 的SCNView 还有一个2d的view ARSKView
         scnView = ARSCNView(frame: view.bounds, options: nil)
+        
+        scnView.delegate = self
+        scnView.session.delegate = self
+        
         view.addSubview(scnView)
         
     }
@@ -26,28 +30,48 @@ class ViewController: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        //ARSession 的配置对象 有两个
+        //ARSessionConfiguration 和 ARWorldTrackingSessionConfiguration
+        //第一个不会水平移动 第二个会  第二个默认带地面识别
         let config = ARWorldTrackingSessionConfiguration()
-        scnView.session.run(config, options: .resetTracking)
-        scnView.delegate = self
-        scnView.session.delegate = self
+        
+        //目前就一个
+        //config.planeDetection = .horizontal
+        
+        //开始运行
+        scnView.session.run(config)
+        
         
         addObject()
         
     }
     
     func addObject() {
+        //初始化一个场景
         let scene = SCNScene()
         
+        //这里写了几个字  具体有哪些内容参照 SceneKit
         let text = SCNText(string: "Yuuki ~~", extrusionDepth: 5)
         text.firstMaterial?.diffuse.contents = UIColor.red
         text.firstMaterial?.specular.contents = UIColor.white
+        
+        //在场景中添加一个节点
         let node = SCNNode(geometry: text)
+        //改变一下位置 不然可能看不到这个
         node.position = SCNVector3(0, -0.1, -2)
+        //更改缩放  不然太大了
         node.scale = SCNVector3(0.05, 0.05, 0.05)
         scene.rootNode.addChildNode(node)
+        
+        //将场景设置给scnView显示
         scnView.scene = scene
+        //默认光源
         scnView.autoenablesDefaultLighting = true
     }
+    
+    /*
+     ARSession 的几个代理方法 具体怎么用还不是很清楚
+     */
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
 //        print("frame \(frame.anchors)")
